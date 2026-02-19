abd5fecfc1830e5131fe3776d6dfc6ad1b4ca996
Leonid Stoyko <gribojor@gmail.com>
2026-02-19 14:43:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import requests
 import folium
 from dotenv import load_dotenv
 from geopy import distance
-from pprint import pprint
 
 
 def fetch_coordinates(apikey, address):
@@ -24,9 +23,9 @@ def fetch_coordinates(apikey, address):
     lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
     return float(lat), float(lon)
 
+
 def get_coffee_closest(coffee_name):
     return coffee_name['distance']
-
 
 
 def main():
@@ -35,44 +34,43 @@ def main():
     if not apikey:
         print("Ошибка: не найден API ключ в файле .env")
         return
-    
+
     with open("coffee.json", "r", encoding="CP1251") as my_file:
         coffee_json = my_file.read()
         file_content = json.loads(coffee_json)
-    
+
     user_address = input("Где вы находитесь? ")
     user_coords = fetch_coordinates(apikey, user_address)
-    
+
     coffee_list = []
-    for coffee in file_content :
+    for coffee in file_content:
         coffee_name = coffee["Name"]
         coffee_lat = float(coffee["Latitude_WGS84"])
         coffee_lon = float(coffee["Longitude_WGS84"])
         coffee_coords = (coffee_lat, coffee_lon)
         dist = distance.distance(user_coords, coffee_coords).km
-    
+
         coffee_list.append({
-            "title" : coffee_name,
-            "distance" : round(dist, 2),
-            "latitude" : (coffee_lat),
-            "longitude" : (coffee_lon)
+            "title": coffee_name,
+            "distance": round(dist, 2),
+            "latitude": (coffee_lat),
+            "longitude": (coffee_lon)
             })
     sort_list_coffe = sorted(coffee_list, key=get_coffee_closest)
-        
-    mape = folium.Map(location=[user_coords[0],user_coords[1]], zoom_start=12)
-   
+
+    mape = folium.Map(location=[user_coords[0], user_coords[1]], zoom_start=12)
     folium.Marker(
-        location=[user_coords[0],user_coords[1]],
+        location=[user_coords[0], user_coords[1]],
         tooltip="Click me!",
         popup="Я тут",
-        icon=folium.Icon(color="red", icon="user"),
+        icon=folium.Icon(color="red", icon="user", prefix="fa"),
     ).add_to(mape)
     for coffee in sort_list_coffe[:5]:
         folium.Marker(
             location=[coffee["latitude"], coffee["longitude"]],
             tooltip=coffee["title"],
             popup=f"Расстояние: {coffee['distance']} км",
-            icon=folium.Icon(color="green", icon="coffee"),
+            icon=folium.Icon(color="green", icon="coffee", prefix="fa"),
         ).add_to(mape)
     mape.save("index.html")
 
